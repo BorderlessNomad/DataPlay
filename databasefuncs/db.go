98c4a07d75f9967e0fa41c135281c3adc2410fd9
Ben Cox <ben@playgen.com>
2013-11-21 10:26:54 +0000

    DB funcs will now foward on the errors they hhit

--- databasefuncs/db.go
+++ databasefuncs/db.go
@@ -6,22 +6,29 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func GetSingleNumberValue(*sql.Rows) string {
+func GetSingleNumberValue(rows *sql.Rows) (out int, e error) {
 	rows.Next()
 	var outputnumber int
-	e = rows.Scan(&outputnumber)
-	if e != nil {
-		return outputnumber
+	ee := rows.Scan(&outputnumber)
+	if e == nil {
+		return outputnumber, ee
 	}
+	return 0, ee
 }
 
-func GetSingleStringValue(*sql.Rows) string {
+func GetSingleStringValue(rows *sql.Rows) (out string, e error) {
 	rows.Next()
 	var outputstr string
-	e = rows.Scan(&outputstr)
-	if e != nil {
-		return outputstr
+	ee := rows.Scan(&outputstr)
+	if e == nil {
+		return outputstr, ee
 	}
+	return "", ee
+}
+
+func GetQueryMonstrosity(*sql.Rows) string {
+	// This one gives you back a csv file of the results
+	return ""
 }
 
 func GetDB() *sql.DB {
