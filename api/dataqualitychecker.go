38136bafe314727efe855c8de56e34ac694cf816
Ben Cox <ben@playgen.com>
2013-11-22 12:33:09 +0000

    caching lib

--- api/dataqualitychecker.go
+++ api/dataqualitychecker.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	cache "../cache"
 	msql "../databasefuncs"
 	"encoding/json"
 	// "fmt"
@@ -17,33 +18,40 @@ type CheckDataQualityResponce struct {
 }
 
 func CheckDataQuality(res http.ResponseWriter, req *http.Request, prams martini.Params, manager *session.SessionManager) string {
+
 	database := msql.GetDB()
 	defer database.Close()
 	if prams["id"] == "" {
 		http.Error(res, "There was no ID request", http.StatusBadRequest)
 	}
 
-	var ckan_url string
-	e := database.QueryRow("SELECT ckan_url FROM `index` WHERE GUID LIKE ? LIMIT 10", prams["id"]+"%").Scan(&ckan_url)
+	test := cache.GetCache("QCheck::" + prams["id"])
+	if test == "" {
+		var ckan_url string
+		e := database.QueryRow("SELECT ckan_url FROM `index` WHERE GUID LIKE ? LIMIT 10", prams["id"]+"%").Scan(&ckan_url)
 
-	url := strings.Replace(strings.Replace(ckan_url, "//", "/", -1), "http:/", "http://", 1)
+		url := strings.Replace(strings.Replace(ckan_url, "//", "/", -1), "http:/", "http://", 1)
 
-	var doc *goq.Document
+		var doc *goq.Document
 
-	if doc, e = goq.NewDocument(url); e != nil {
-		http.Error(res, "Unable to fetch page", http.StatusServiceUnavailable)
+		if doc, e = goq.NewDocument(url); e != nil {
+			http.Error(res, "Unable to fetch page", http.StatusServiceUnavailable)
+		}
+
+		var count int
+
+		doc.Find(".icon-star").Each(func(i int, s *goq.Selection) {
+			count++
+		})
+
+		returnobj := CheckDataQualityResponce{
+			Amount:  count,
+			Request: prams["id"],
+		}
+		b, _ := json.Marshal(returnobj)
+		cache.SetCache("QCheck::"+prams["id"], string(b[:]))
+		return string(b[:])
+	} else {
+		return test
 	}
-
-	var count int
-
-	doc.Find(".icon-star").Each(func(i int, s *goq.Selection) {
-		count++
-	})
-
-	returnobj := CheckDataQualityResponce{
-		Amount:  count,
-		Request: prams["id"],
-	}
-	b, _ := json.Marshal(returnobj)
-	return string(b[:])
 }
