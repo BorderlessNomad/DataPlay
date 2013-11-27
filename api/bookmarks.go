8afaabf10037c4d5e485510db3c6fa9052de327f
Ben Cox <ben@playgen.com>
2013-11-27 11:54:23 +0000

    Added bookmarks lib

---
+++ api/bookmarks.go
@@ -0,0 +1,49 @@
+package api
+
+import (
+	msql "../databasefuncs"
+	"encoding/json"
+	"fmt"
+	"github.com/codegangsta/martini"
+	"github.com/mattn/go-session-manager"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func SetBookmark(res http.ResponseWriter, req *http.Request, prams martini.Params) string {
+	// Okay this is suppose to be a POST request that will spit back out a UUID
+	database := msql.GetDB()
+	defer database.Close()
+	jsondata := req.FormValue("data")
+	_, e := database.Query("INSERT INTO `priv_shares` (`jsoninfo`) VALUES (?);", jsondata)
+	if e != nil {
+		panic(e)
+	}
+	r, e := database.QueryRow("SELECT LAST_INSERT_ID()")
+	if e != nil {
+		panic(e)
+	}
+	var id int
+	r.Scan(&id)
+	return fmt.Sprint("%d", id)
+}
+
+func GetBookmark(res http.ResponseWriter, req *http.Request, prams martini.Params) string {
+	database := msql.GetDB()
+	defer database.Close()
+
+	if prams["id"] == "" {
+		http.Error(res, "You didnt give me a id to lookup", 404)
+	}
+	var input int
+	var e error
+	input, e = strconv.ParseInt(prams["id"], 10, 16)
+	if e == nil {
+		http.Error(res, "Thats not a number.", 500)
+	}
+	rows, e := database.QueryRow("SELECT jsoninfo FROM `priv_shares` WHERE shareid = ?", input)
+	var output string
+	rows.Scan(&output)
+	return output
+}
