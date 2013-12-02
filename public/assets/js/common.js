01434eff58c62c195d66520358757636d9598819
Ben Cox <ben@playgen.com>
2013-12-02 09:47:02 +0000

    Sanify the common.js file

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -1,30 +1,33 @@
 function parseChartData(data, x, y) {
-	var DataPool = [];
-	for (var i = 0; i < data.length; i++) {
-		DataPool.push([ parseFloat(data[i][x]) , parseFloat(data[i][y]) ]);
-	}
-	return DataPool;
+    var DataPool = [];
+    for (var i = 0; i < data.length; i++) {
+        DataPool.push([parseFloat(data[i][x]), parseFloat(data[i][y])]);
+    }
+    return DataPool;
 }
 
 function saveUserDefaults(guid, x, y) {
-	$.ajax({
+    $.ajax({
         type: "POST",
         url: '/api/setdefaults/' + guid,
         async: true,
-        data: "data="+JSON.stringify({ x: x, y: y }),
-        success: function (resp) {
-       		//console.log(resp.result);
+        data: "data=" + JSON.stringify({
+            x: x,
+            y: y
+        }),
+        success: function(resp) {
+            //console.log(resp.result);
         },
-        error: function (err) {
-        	console.log(err);
+        error: function(err) {
+            console.log(err);
         }
     });
 }
 
 function getUserDefaults(guid, x, y, cb) {
-	$.getJSON( "/api/getdefaults/" + guid, function( data ) {
-		$(x).val(data.x);
-		$(y).val(data.y);
+    $.getJSON("/api/getdefaults/" + guid, function(data) {
+        $(x).val(data.x);
+        $(y).val(data.y);
         cb && cb();
-	});
+    });
 }
