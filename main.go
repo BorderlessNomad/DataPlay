2083382fcbed17c9703b1f8f04d801b675df0c9a
Ben Cox <ben@playgen.com>
2013-12-05 13:56:32 +0000

    Add the bubble route

--- main.go
+++ main.go
@@ -54,6 +54,9 @@ func main() {
 	m.Get("/viewbookmark/:id", func(res http.ResponseWriter, req *http.Request) {
 		http.ServeFile(res, req, "public/bookmarked.html")
 	})
+	m.Get("/bubble/:id", func(res http.ResponseWriter, req *http.Request) {
+		http.ServeFile(res, req, "public/bubble.html")
+	})
 	m.Get("/search/overlay", func(res http.ResponseWriter, req *http.Request) {
 		http.ServeFile(res, req, "public/search.html")
 	})
