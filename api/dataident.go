a5beb6c2e78e91bf680097bddcfcadc69786738f
Ben Cox <ben@playgen.com>
2013-12-06 14:33:25 +0000

    functioned a few things out started off on a "hard core" intentifier

--- api/dataident.go
+++ api/dataident.go
@@ -53,14 +53,20 @@ func IdentifyTable(res http.ResponseWriter, req *http.Request, prams martini.Par
 	return string(b[:])
 }
 
-func ParseCreateTableSQL(input string) []ColType {
+func BuildREArrayForCreateTable(input string) []string {
 	re := ".*?(`.*?`).*?((?:[a-z][a-z]+))" // http://i.imgur.com/dkbyB.jpg
 	var sqlRE = regexp.MustCompile(re)
+	results := sqlRE.FindStringSubmatch(input)
+	return results
+}
+
+func ParseCreateTableSQL(input string) []ColType {
 	returnerr := make([]ColType, 0) // Setup the array that I will be append()ing to.
 	SQLLines := strings.Split(input, "\n")
+
 	for c, line := range SQLLines {
 		if c != 0 { // Clipping off the create part since its useless for me.
-			results := sqlRE.FindStringSubmatch(line)
+			results := BuildREArrayForCreateTable(line)
 			if len(results) == 3 {
 				NewCol := ColType{
 					Name:    results[1],
@@ -71,4 +77,50 @@ func ParseCreateTableSQL(input string) []ColType {
 		}
 	}
 	return returnerr
+}
+
+type SuggestionResponce struct {
+	Request string
+}
+
+func SuggestColType(res http.ResponseWriter, req *http.Request, prams martini.Params) string {
+	database := msql.GetDB()
+	defer database.Close()
+	if prams["id"] == "" {
+		http.Error(res, "There was no ID request", http.StatusBadRequest)
+	}
+
+	var tablename string
+	database.QueryRow("SELECT TableName FROM `priv_onlinedata` WHERE GUID = ? LIMIT 1", prams["id"]).Scan(&tablename)
+	if tablename == "" {
+		http.Error(res, "Could not find that table", http.StatusNotFound)
+		return ""
+	}
+
+	var createcode string
+	database.QueryRow("SHOW CREATE TABLE "+tablename).Scan(&tablename, &createcode)
+	if createcode == "" {
+		http.Error(res, `Uhh, That table does not seem to acutally exist.
+		this really should not happen. 
+		Check if someone have been messing around in the database.`, http.StatusBadRequest)
+		return ""
+	}
+	return "" // Shut up go
+}
+
+func CheckIfColExists(createcode string, targettable string) bool {
+
+	SQLLines := strings.Split(input, "\n")
+
+	for c, line := range SQLLines {
+		if c != 0 { // Clipping off the create part since its useless for me.
+			results := BuildREArrayForCreateTable(line)
+			if len(results) == 3 {
+				if results[1] == targettable {
+					return true
+				}
+			}
+		}
+	}
+	return false
 }
