553bec742cbf8f88e3e6280d8d35f932d4755443
Ben Cox <ben@playgen.com>
2013-12-06 16:04:16 +0000

    Fix compile wines

--- api/dataident.go
+++ api/dataident.go
@@ -129,12 +129,12 @@ func SuggestColType(res http.ResponseWriter, req *http.Request, prams martini.Pa
 		http.Error(res, "You have requested a col that does not exist. Please avoid doing this in the future.", http.StatusBadRequest)
 		return "" // Shut up go
 	}
-
+	return "This isnt suppose to happen"
 }
 
 func CheckIfColExists(createcode string, targettable string) bool {
 
-	SQLLines := strings.Split(input, "\n")
+	SQLLines := strings.Split(createcode, "\n")
 
 	for c, line := range SQLLines {
 		if c != 0 { // Clipping off the create part since its useless for me.
