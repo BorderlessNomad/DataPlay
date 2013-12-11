40686a8f49ac760378c29dbc41880ea9b515de8d
Ben Cox <ben@playgen.com>
2013-12-11 11:38:32 +0000

    It now really works
    Some WTF notes. Okay so the whole thing runs shit slow BUT
    if you add
    +	runtime.GOMAXPROCS(3)
    then suddenly it runs like a fucking rocket... WTF?!

--- tools/ckandownloader/main.go
+++ tools/ckandownloader/main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"runtime"
 	"strings"
 )
 
 func main() {
+	runtime.GOMAXPROCS(3)
 	if len(os.Args) == 2 {
 		listfile := string(os.Args[1])
 		fileContents, e := ioutil.ReadFile(listfile)
@@ -15,8 +17,8 @@ func main() {
 			// split the file into lines.
 			lines := strings.Split(string(fileContents), "\n")
 			for i, line := range lines {
-				fmt.Printf("Downloading dataset %d/%d \n", i, len(lines))
-				ImportAllDatasets(line, string(i))
+				fmt.Printf("!!Downloading dataset %d/%d \n", i, len(lines))
+				ImportAllDatasets(line, fmt.Sprintf("%d", i))
 			}
 		} else {
 			fmt.Println("Failed to load that file.")
--- tools/ckandownloader/scrapers.go
+++ tools/ckandownloader/scrapers.go
@@ -7,27 +7,32 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+
+	// "time"
 )
 
 func ImportAllDatasets(url string, guid string) {
 	// This call is desgined to fetch all of the links that are needed to be downloaded (on data.gov.uk)
 	// And then fire off go routines to go and fetch them.
+	url = strings.Replace(url, "\r", "", -1)
 	var e error
 	var doc *goq.Document
-	fmt.Println("Loading URL", url)
-	if doc, e = goq.NewDocument(url); e != nil {
-		panic("Unable to fetch page!")
-	}
-	doc.Find(".dropdown-menu").Each(func(i int, s *goq.Selection) {
-		s.Find("a").Each(func(i int, s *goq.Selection) {
-			url, exists := s.Attr("href")
-			html, _ := s.Html()
-			if exists && strings.Contains(html, "icon-download-alt") {
-				fmt.Println(url)
-				go DownloadDataset(url, guid)
-			}
+	fmt.Println("Loading URL '" + url + "'")
+	if doc, e = goq.NewDocument(url); e == nil {
+		// fmt.Println(doc.Html())
+		// time.Sleep(time.Second * 1)
+		doc.Find(".dropdown-menu").Each(func(i int, s *goq.Selection) {
+			s.Find("a").Each(func(i int, s *goq.Selection) {
+				url, exists := s.Attr("href")
+				html, _ := s.Html()
+				// fmt.Println(html)
+				if exists && strings.Contains(html, "icon-download-alt") {
+					fmt.Println(url)
+					go DownloadDataset(url, guid)
+				}
+			})
 		})
-	})
+	}
 }
 
 func DownloadDataset(url string, guid string) {
@@ -35,14 +40,25 @@ func DownloadDataset(url string, guid string) {
 	// it will make a SQL table for it and then put the fact that the data exists in the online table allowing the client
 	// to move along and to ensure that it wont be downloaded twice.
 	fmt.Println("Downloading dataset", url)
-	response, _ := http.Get(url)
-	fmt.Println(response.Header)
-	if response.Header.Get("Content-Type") == "text/csv" {
+	response, e := http.Get(url)
+	if e == nil {
+		fmt.Println(response.Header)
+		if response.Header.Get("Content-Type") == "text/csv" {
 
-		full, _ := ioutil.ReadAll(response.Body)
-		hash := sha1.New()
-		hash.Write([]byte(url))
-		ioutil.WriteFile("./"+guid+"_"+fmt.Sprintf("%x", hash.Sum(nil))+".csv", full, 0667)
+			full, _ := ioutil.ReadAll(response.Body)
+			hash := sha1.New()
+			hash.Write([]byte(url))
+			ioutil.WriteFile("./"+guid+"_"+fmt.Sprintf("%x", hash.Sum(nil))+".csv", full, 0667)
+		} else if response.Header.Get("Content-Type") == "application/vnd.ms-excel" {
+			full, _ := ioutil.ReadAll(response.Body)
+			hash := sha1.New()
+			hash.Write([]byte(url))
+			ioutil.WriteFile("./"+guid+"_"+fmt.Sprintf("%x", hash.Sum(nil))+".xlsx", full, 0667)
+		} else {
+			full, _ := ioutil.ReadAll(response.Body)
+			hash := sha1.New()
+			hash.Write([]byte(url))
+			ioutil.WriteFile("./"+guid+"_"+fmt.Sprintf("%x", hash.Sum(nil))+".dunno", full, 0667)
+		}
 	}
-
 }
