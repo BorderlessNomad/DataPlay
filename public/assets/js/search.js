6b1a2e6b3db8a591e2edff570ab93c35de7d10b1
Ben Cox <ben@playgen.com>
2013-12-17 16:05:15 +0000

    Make sure search does not overflow. Fix the last item being blank

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -8,13 +8,15 @@ $(document).ready(function() {
                 $('#ResultsTable').empty();
                 $('#ResultsTable').append("<td><tr>Title</tr><tr>Guid</tr></td>");
                 for (var i = data.length - 1; i >= 0; i--) {
-                    $('#ResultsTable').append("<tr id=\"" + data[i].GUID + "\"><td>" + data[i].Title + "</td><td id=\"Note" + data[i].GUID + "\">" + data[i].GUID + "</td></tr>");
-                    $('#' + data[i].GUID).addClass("success");
-                    if (guid !== "overlay")
-                        $('#Note' + data[i].GUID).html("<a href=\"/view/" + data[i].GUID + "\">View &raquo;</a>&nbsp;<a href=\"/grid/" + data[i].GUID + "\">Grid &raquo;</a>");
-                    else
-                        $('#Note' + data[i].GUID).html("<a href=\"/overlay/" + data[i].GUID + "\">Overlay &raquo;</a>");
-                    console.log(data);
+                	if(data[i].Title != "") {
+	                    $('#ResultsTable').append("<tr id=\"" + data[i].GUID + "\"><td style=\"width: 70%;\">" + data[i].Title + "</td><td id=\"Note" + data[i].GUID + "\">" + data[i].GUID + "</td></tr>");
+	                    $('#' + data[i].GUID).addClass("success");
+	                    if (guid !== "overlay")
+	                        $('#Note' + data[i].GUID).html("<a href=\"/view/" + data[i].GUID + "\">View &raquo;</a>&nbsp;<a href=\"/grid/" + data[i].GUID + "\">Grid &raquo;</a>");
+	                    else
+	                        $('#Note' + data[i].GUID).html("<a href=\"/overlay/" + data[i].GUID + "\">Overlay &raquo;</a>");
+	                    console.log(data);
+	                }
                 }
             });
         }
