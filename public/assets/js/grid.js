ffe0548e0f6b80fed86ae0abc58fb9be857fd0bf
Ben Cox <ben@playgen.com>
2013-12-18 10:31:36 +0000

    Added more sane grid view!

--- public/assets/js/grid.js
+++ public/assets/js/grid.js
@@ -13,6 +13,16 @@ function LightUpBookmarks() {
     // do nothing
 }
 
+function ProabblyANumber(name, identdata) {
+    var RealArray = identdata.Cols;
+    for (var i = 0; i < RealArray.length; i++) {
+        if(RealArray[i].Name === name && (RealArray[i].Sqltype === "float" || RealArray[i].Sqltype === "bigint" || RealArray[i].Sqltype === "int")) {
+            return true;
+        }
+    }
+    return false;
+}
+
 $(document).ready(function() {
     // First thing we need to prepare the table. I'm going to make a 3x3 grid
 
@@ -59,9 +69,30 @@ $(document).ready(function() {
                 }
             }
             var Entropy = Math.pow(2, Keys.length - 1);
+            var GridMembers = [];
+            var k1 = 0;
+            var k2 = 0;
+            for (var i = 0; i < Entropy; i++) {
+                if(ProabblyANumber(Keys[k2],tabledata)) {
+                    GridMembers.push({
+                        x: Keys[k1],
+                        y: Keys[k2]
+                    });
+                }
+                k1++;
+                if (k1 === k2) {
+                    k1++;
+                }
+                if (k1 === Keys.length) {
+                    k1 = 0;
+                    k2++;
+                }
+            }
+            console.log(GridMembers)
+
             var TableHandle = $('#GridTable');
             var count = 0;
-            for (var i = 0; i < Entropy / 3; i++) {
+            for (var i = 0; i < GridMembers.length / 3; i++) {
                 $('#GridTable').append(
                     '<tr>' +
                     '<td><div class="gridCell" id="Cell' + count + '"></div></td>' +
@@ -71,10 +102,8 @@ $(document).ready(function() {
                 count = count + 3;
             }
             var CellCount = 0;
-            var k1 = 0;
-            var k2 = 0;
             var DCG = [];
-            for (var i = 0; i < Entropy; i++) {
+            for (var i = 0; i < GridMembers.length; i++) {
                 DCG[i] = new PGLinesChart(
                     "#Cell" + i, {
                         top: 5,
@@ -82,9 +111,9 @@ $(document).ready(function() {
                         bottom: 5,
                         left: 5
                     },
-                    parseChartData(data, Keys[k1], Keys[k2]), {
-                        x: Keys[k1],
-                        y: Keys[k2]
+                    parseChartData(data, GridMembers[i].x, GridMembers[i].y), {
+                        x: GridMembers[i].x,
+                        y: GridMembers[i].y
                     },
                     null
                 );
