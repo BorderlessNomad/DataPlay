6db52541512404291897f46772521f73419f8d00
Unai <unai@playgen.com>
2013-12-31 15:19:12 +0000

    another less file to compile

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
 echo BUILDING JS / CSS
-call node_modules\.bin\coffee -c -o public\js src\coffee && call node_modules\.bin\lessc src\less\layout.less public\css\layout.css && call node_modules\.bin\lessc src\less\charts.less public\css\charts.css && call node_modules\.bin\lessc src\less\signin.less public\css\signin.css
+call node_modules\.bin\coffee -c -o public\js src\coffee && call node_modules\.bin\lessc src\less\layout.less public\css\layout.css && call node_modules\.bin\lessc src\less\maptest.less public\css\maptest.css && call node_modules\.bin\lessc src\less\charts.less public\css\charts.css && call node_modules\.bin\lessc src\less\signin.less public\css\signin.css
 echo BUILDING GOGRAM
 go build && datacon
