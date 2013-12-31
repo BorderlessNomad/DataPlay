1a12fc4c385cae035b77c24d8cdaea9a7c99f9cb
Ben Cox <ben@playgen.com>
2013-12-31 09:58:02 +0000

    Glue all the CSS & JS compile together in the build

--- start.bat
+++ start.bat
@@ -1,5 +1,2 @@
-call node_modules\.bin\coffee -c -o public\js src\coffee
-call node_modules\.bin\lessc src\less\layout.less public\css\layout.css
-call node_modules\.bin\lessc src\less\charts.less public\css\charts.css
-call node_modules\.bin\lessc src\less\signin.less public\css\signin.css
+call node_modules\.bin\coffee -c -o public\js src\coffee && call node_modules\.bin\lessc src\less\layout.less public\css\layout.css && call node_modules\.bin\lessc src\less\charts.less public\css\charts.css && call node_modules\.bin\lessc src\less\signin.less public\css\signin.css
 go build && datacon
