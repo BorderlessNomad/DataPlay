0309f03c8eb0f5f9cfdd00dd1b3046b7e2b13e8d
Ben Cox <ben@playgen.com>
2014-02-03 09:50:00 +0000

    SubModuled out "OpenLayers"

--- start.bat
+++ start.bat
@@ -3,5 +3,6 @@ echo BUILDING DEPENDENCIES
 call npm install
 echo BUILDING JS / CSS
 call node_modules\.bin\coffee -c -o public\js src\coffee && call node_modules\.bin\lessc src\less\layout.less public\css\layout.css && call node_modules\.bin\lessc src\less\maptest.less public\css\maptest.css && call node_modules\.bin\lessc src\less\charts.less public\css\charts.css && call node_modules\.bin\lessc src\less\signin.less public\css\signin.css
+cp public\lib\openlayers\lib\OpenLayers.js public\lib\dependencies\js\OpenLayers.js
 echo BUILDING GOGRAM
 go build && datacon
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo 'BUILDING DEPENDENCIES'
 npm install
 echo 'BUILDING JS/CSS'
@@ -6,6 +8,7 @@ node_modules/.bin/lessc src/less/layout.less public/css/layout.css
 node_modules/.bin/lessc src/less/signin.less public/css/signin.css
 node_modules/.bin/lessc src/less/charts.less public/css/charts.css
 node_modules/.bin/lessc src/less/maptest.less public/css/maptest.css
+cp public/lib/openlayers/lib/OpenLayers.js public/lib/dependencies/js/OpenLayers.js
 echo 'BUILDING GOGRAM'
 go build
 ./datacon
