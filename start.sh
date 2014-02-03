d1065eef35bbfa1f2013aa4ab49fcbf7beb056f7
Ben Cox <ben@playgen.com>
2014-02-03 10:42:20 +0000

    Script Lint

--- start.sh
+++ start.sh
@@ -8,11 +8,12 @@ node_modules/.bin/lessc src/less/layout.less public/css/layout.css
 node_modules/.bin/lessc src/less/signin.less public/css/signin.css
 node_modules/.bin/lessc src/less/charts.less public/css/charts.css
 node_modules/.bin/lessc src/less/maptest.less public/css/maptest.css
-#cp -r public/lib/openlayers/lib/ public/lib/dependencies/js/
+
 cd public/lib/openlayers/build
 python build.py
 cp -r OpenLayers.js ../../../../public/lib/dependencies/js/
 cd ../../../../
+
 echo 'BUILDING GOGRAM'
 go build
 ./datacon
