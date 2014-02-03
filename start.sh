ee156e044e00461715104e87698339100f064a67
Ben Cox <ben@playgen.com>
2014-02-03 09:57:06 +0000

    Fix the build scripts

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ node_modules/.bin/lessc src/less/layout.less public/css/layout.css
 node_modules/.bin/lessc src/less/signin.less public/css/signin.css
 node_modules/.bin/lessc src/less/charts.less public/css/charts.css
 node_modules/.bin/lessc src/less/maptest.less public/css/maptest.css
-cp public/lib/openlayers/lib/OpenLayers.js public/lib/dependencies/js/OpenLayers.js
+cp -r public/lib/openlayers/lib/ public/lib/dependencies/js/
 echo 'BUILDING GOGRAM'
 go build
 ./datacon
