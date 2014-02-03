e1e07818f1b8ff332aadc45f8c363134e683f62b
Ben Cox <ben@playgen.com>
2014-02-03 10:08:50 +0000

    Aaaand the bat version too

--- start.bat
+++ start.bat
@@ -3,6 +3,11 @@ echo BUILDING DEPENDENCIES
 call npm install
 echo BUILDING JS / CSS
 call node_modules\.bin\coffee -c -o public\js src\coffee && call node_modules\.bin\lessc src\less\layout.less public\css\layout.css && call node_modules\.bin\lessc src\less\maptest.less public\css\maptest.css && call node_modules\.bin\lessc src\less\charts.less public\css\charts.css && call node_modules\.bin\lessc src\less\signin.less public\css\signin.css
-cp -r public/lib/openlayers/lib/ public/lib/dependencies/js/
+
+cd public/lib/openlayers/build
+python build.py
+cp -r OpenLayers.js ../../../../public/lib/dependencies/js/
+cd ../../../../
+
 echo BUILDING GOGRAM
 go build && datacon
