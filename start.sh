324cf9b4000b2e47c566a7d134a7169559078da4
Ben Cox <ben@playgen.com>
2014-02-07 09:27:21 +0000

    Added &&'s to the start.sh file to ensure that errors dont get ignored

--- start.sh
+++ start.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-echo 'BUILDING DEPENDENCIES'
-npm install
-echo 'BUILDING JS/CSS'
-node_modules/.bin/coffee -c -m -o public/js src/coffee
-node_modules/.bin/lessc src/less/layout.less public/css/layout.css
-node_modules/.bin/lessc src/less/signin.less public/css/signin.css
-node_modules/.bin/lessc src/less/charts.less public/css/charts.css
-node_modules/.bin/lessc src/less/maptest.less public/css/maptest.css
+echo 'BUILDING DEPENDENCIES' &&
+npm install &&
+echo 'BUILDING JS/CSS' &&
+node_modules/.bin/coffee -c -m -o public/js src/coffee &&
+node_modules/.bin/lessc src/less/layout.less public/css/layout.css &&
+node_modules/.bin/lessc src/less/signin.less public/css/signin.css &&
+node_modules/.bin/lessc src/less/charts.less public/css/charts.css &&
+node_modules/.bin/lessc src/less/maptest.less public/css/maptest.css &&
 
-cd public/lib/openlayers/build
-python build.py
-cp -r OpenLayers.js ../../../../public/lib/dependencies/js/
-cd ../../../../
+cd public/lib/openlayers/build &&
+python build.py &&
+cp -r OpenLayers.js ../../../../public/lib/dependencies/js/ &&
+cd ../../../../ &&
 
-echo 'BUILDING GOGRAM'
-go build
+echo 'BUILDING GOGRAM' &&
+go build &&
 ./datacon
