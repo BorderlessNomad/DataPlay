2b6e89f0a2ec9cc768638447f78e835465f7512e
Ben Cox <ben@playgen.com>
2014-02-11 09:09:29 +0000

    Improved speed on the Index builder.

--- tools/makesearch_index/main.go
+++ tools/makesearch_index/main.go
@@ -43,13 +43,21 @@ func IndexTable(job ScanJob, db *sql.DB) {
 	q, _ := db.Query(fmt.Sprintf("SELECT `%s` FROM `%s`", job.X, job.TableName))
 	// xarray := make([]string, 0)
 	checkingdict := make(map[string]bool)
+	UpdateQ, e := db.Prepare("UPDATE `priv_stringsearch` SET `count`=`count`+1 WHERE  `tablename`=? AND `x`=? AND `value`=? LIMIT 1;")
+	if e != nil {
+		panic(e)
+	}
+	InsertQ, e := db.Prepare("INSERT INTO `priv_stringsearch` (`tablename`, `x`, `value`) VALUES (?, ?, ?);")
+	if e != nil {
+		panic(e)
+	}
 	for q.Next() {
 		var strout string
 		q.Scan(&strout)
 		if checkingdict[strout] {
-			db.Exec("UPDATE `priv_stringsearch` SET `count`=`count`+1 WHERE  `tablename`=? AND `x`=? AND `value`=? LIMIT 1;", job.TableName, job.X, strout)
+			UpdateQ.Exec(job.TableName, job.X, strout)
 		} else {
-			db.Exec("INSERT INTO `priv_stringsearch` (`tablename`, `x`, `value`) VALUES (?, ?, ?);", job.TableName, job.X, strout)
+			InsertQ.Exec(job.TableName, job.X, strout)
 			checkingdict[strout] = true
 		}
 		// xarray = append(xarray, strout)
