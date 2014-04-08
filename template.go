8bdf8ec7931ef4f32578bbbddb481eaf7e5e058e
Ben Cox <ben@playgen.com>
2014-04-08 09:48:48 +0100

    Added docs to Template.go

--- template.go
+++ template.go
@@ -13,13 +13,13 @@ type Page struct {
 	Custom map[string]string
 }
 
-var h template.HTML
-var n template.HTML
-var f template.HTML
+var h template.HTML // Header
+var n template.HTML // Navbar
+var f template.HTML // Footer
 
 func ApplyTemplate(FileName string, Inject string, res http.ResponseWriter) {
 	b, _ := ioutil.ReadFile(FileName)
-	t := template.New("TPage")
+	t := template.New("TPage") // Not sure why we have to name these, but we are basically forced to.
 	t.Parse(string(b))
 	t.Execute(res, Inject)
 }
@@ -30,6 +30,11 @@ func renderTemplate(fileName string, custom map[string]string, res http.Response
 	t.Execute(res, p)
 }
 
+// GoTemplates need to be compiled. We do this on start so we don't have to read them over and over,
+// this does mean however that you need to restart the server when you make changes to the below templates
+// * public/templates/header.html
+// * public/templates/navbar.html
+// * public/templates/footer.html
 func initTemplates() {
 	hf, _ := ioutil.ReadFile("public/templates/header.html")
 	h = template.HTML(hf)
