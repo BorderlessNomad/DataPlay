a82f47c67b9417e6c195eb32937f7f28c90517e6
Lex Robinson <lex@playgen.com>
2014-05-23 11:26:42 +0100

    Move the src dir to www-src

--- start.sh
+++ start.sh
@@ -3,11 +3,11 @@
 echo 'BUILDING DEPENDENCIES' &&
 npm install &&
 echo 'BUILDING JS/CSS' &&
-node_modules/.bin/coffee -c -m -o public/js src/coffee &&
-node_modules/.bin/lessc src/less/layout.less public/css/layout.css &&
-node_modules/.bin/lessc src/less/signin.less public/css/signin.css &&
-node_modules/.bin/lessc src/less/charts.less public/css/charts.css &&
-node_modules/.bin/lessc src/less/maptest.less public/css/maptest.css &&
+node_modules/.bin/coffee -c -m -o public/js www-src/coffee &&
+node_modules/.bin/lessc www-src/less/layout.less public/css/layout.css &&
+node_modules/.bin/lessc www-src/less/signin.less public/css/signin.css &&
+node_modules/.bin/lessc www-src/less/charts.less public/css/charts.css &&
+node_modules/.bin/lessc www-src/less/maptest.less public/css/maptest.css &&
 
 if [ ! -f public/lib/openlayers/build/OpenLayers.js ]; then
 	cd public/lib/openlayers/build &&
