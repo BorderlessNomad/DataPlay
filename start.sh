672a0021e17b70657d19c4ed3e496928ddca91cb
Lex Robinson <lex@playgen.com>
2014-05-23 15:02:34 +0100

    Improve building of openlayers a little

--- start.sh
+++ start.sh
@@ -2,19 +2,15 @@
 
 echo 'BUILDING DEPENDENCIES' &&
 npm install &&
-npm install -g grunt-cli
 echo 'BUILDING JS/CSS' &&
 grunt &&
 
 if [ ! -f public/lib/openlayers/build/OpenLayers.js ]; then
-	cd public/lib/openlayers/build &&
+	mkdir -p public/lib/dependencies/js/ &&
+	pushd public/lib/openlayers/build &&
 	python build.py &&
-	mkdir ../../../../public/ &&
-	mkdir ../../../../public/lib/ &&
-	mkdir ../../../../public/lib/dependencies/ &&
-	mkdir ../../../../public/lib/dependencies/js/ &&
-	cp -r OpenLayers.js ../../../../public/lib/dependencies/js/
-	cd ../../../../
+	cp -r OpenLayers.js ../../dependencies/js/ &&
+	popd
 fi
 
 echo 'BUILDING GOGRAM' &&
