39841b4008114f34162500aadc5235aecbe57117
Glyn Dimond <glyn@playgen.com>
2014-06-26 16:09:24 +0100

    improved runmethod to take n parameters

--- src/dataplay/queue.go
+++ src/dataplay/queue.go
@@ -4,28 +4,16 @@ import (
 	"reflect"
 )
 
-type Q struct{}
+type Queue struct{}
 
 func RunMethod(name string, params ...interface{}) []reflect.Value {
-	var q Q
-	var r []reflect.Value
-	p := len(params)
+	var q Queue
+	args := []reflect.Value{}
 
-	switch p {
-	case 0:
-		r = reflect.ValueOf(&q).MethodByName(name).Call([]reflect.Value{})
-	case 1:
-		r = reflect.ValueOf(&q).MethodByName(name).Call([]reflect.Value{reflect.ValueOf(params[0])})
-	case 2:
-		r = reflect.ValueOf(&q).MethodByName(name).Call([]reflect.Value{reflect.ValueOf(params[0]), reflect.ValueOf(params[1])})
-	case 3:
-		r = reflect.ValueOf(&q).MethodByName(name).Call([]reflect.Value{reflect.ValueOf(params[0]), reflect.ValueOf(params[1]), reflect.ValueOf(params[2])})
-	case 4:
-		r = reflect.ValueOf(&q).MethodByName(name).Call([]reflect.Value{reflect.ValueOf(params[0]), reflect.ValueOf(params[1]), reflect.ValueOf(params[2]), reflect.ValueOf(params[3])})
-	case 5:
-		r = reflect.ValueOf(&q).MethodByName(name).Call([]reflect.Value{reflect.ValueOf(params[0]), reflect.ValueOf(params[1]), reflect.ValueOf(params[2]), reflect.ValueOf(params[3]), reflect.ValueOf(params[4])})
-	default:
-		panic("too many arguments")
+	for i, _ := range params {
+		args = append(args, reflect.ValueOf(params[i]))
 	}
+
+	r := reflect.ValueOf(&q).MethodByName(name).Call(args)
 	return r
 }
