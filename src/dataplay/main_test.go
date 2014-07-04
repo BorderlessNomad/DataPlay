4f668783a4f9764e6a3545cf8bd18bfdbe339f5c
Glyn Dimond <glyn@playgen.com>
2014-07-04 10:42:57 +0100

    Added sendToQueue test

--- src/dataplay/main_test.go
+++ src/dataplay/main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	//"github.com/codegangsta/martini"
+	. "github.com/smartystreets/goconvey/convey"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -24,4 +26,15 @@ func TestJsonApiHandler(t *testing.T) {
 	request, _ := http.NewRequest("GET", "http://dataplay.com/api/user", nil)
 	response := httptest.NewRecorder()
 	JsonApiHandler(response, request)
+}
+
+func TestSendToQueue(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/", nil)
+	response := httptest.NewRecorder()
+	reqstr, methstr := "", ""
+	params := map[string]string{"": ""}
+	result := sendToQueue(response, request, params, reqstr, methstr)
+	Convey("Should return response channel", t, func() {
+		So(result, ShouldEqual, "[]")
+	})
 }
