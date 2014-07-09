a83e138d05ee9f3c608140aa1093b28f7ebe39b1
Glyn Dimond <glyn@playgen.com>
2014-07-09 16:16:15 +0100

    Added new datasuggestion file - basis for finding random correlations. Added test cases for this. Added Extractdata func to dataident. Added new correlation table struct to models. Inserted compilable comments to datacorrelation and dataident.

--- src/dataplay/dataident.go
+++ src/dataplay/dataident.go
@@ -44,7 +44,15 @@ type PossibleCombo struct {
 	Tables []string
 }
 
-// This function checks to see if the data has been imported yet or still is in need of importing
+/**
+ * @brief This function checks to see if the data has been imported yet or still is in need of importing
+ * @details
+ *
+ * @param http
+ * @param http
+ * @param martini
+ * @return
+ */
 func IdentifyTable(res http.ResponseWriter, req *http.Request, params martini.Params) string {
 	if params["id"] == "" {
 		http.Error(res, "There was no ID request", http.StatusBadRequest)
@@ -61,7 +69,13 @@ func IdentifyTable(res http.ResponseWriter, req *http.Request, params martini.Pa
 	return string(b)
 }
 
-// This fetches a array of all the col names and their types.
+/**
+ * @brief This fetches an array of all the col names and their types.
+ * @details
+ *
+ * @param string
+ * @return
+ */
 func FetchTableCols(guid string) (output []ColType) {
 	if guid == "" {
 		return output
@@ -76,6 +90,17 @@ func FetchTableCols(guid string) (output []ColType) {
 	results := GetSQLTableSchema(tablename)
 
 	return results
+}
+
+func ExtractData(guid string, col string) []float64 {
+	var result []float64
+
+	if guid == "" || col == "" {
+		return result
+	}
+
+	DB.Table(guid).Pluck(col, &result)
+	return result
 }
 
 func HasTableGotLocationData(datasetGUID string) string {
