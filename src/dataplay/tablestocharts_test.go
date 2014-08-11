f9f3ce3c10365875e57e025ba1f36dc7160faaff
Glyn Dimond <glyn@playgen.com>
2014-08-11 11:58:56 +0100

    Added GetChart functions for getting single chart

--- src/dataplay/tablestocharts_test.go
+++ src/dataplay/tablestocharts_test.go
@@ -35,3 +35,20 @@ func TestGetRelatedChartsHttp(t *testing.T) {
 		So(result, ShouldNotBeNil)
 	})
 }
+
+func TestGetChart(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/", nil)
+	req.Header.Set("X-API-SESSION", "00TK6wuwwj1DmVDtn8mmveDMVYKxAJKLVdghTynDXBd62wDqGUGlAmEykcnaaO66")
+	res := httptest.NewRecorder()
+	params := map[string]string{
+		"user":      "1",
+		"tablename": "gold",
+		"type":      "line",
+		"x":         "price",
+		"y":         "date",
+	}
+	Convey("Should return chartlist", t, func() {
+		result := GetChartHttp(res, req, params)
+		So(result, ShouldNotBeNil)
+	})
+}
