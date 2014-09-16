4d46c1755e00fdd88abd627ebc90c26ecc6488ae
Glyn Dimond <glyn@playgen.com>
2014-09-16 10:03:41 +0100

    Political Overview now returning full graphs.

--- src/dataplay/politicaloverview.go
+++ src/dataplay/politicaloverview.go
@@ -29,30 +29,16 @@ type DateID struct {
 	Date time.Time
 }
 
-type KeyEnt struct {
-	Name string
+type DatedTerm struct {
+	Term string
 	Date time.Time
 	ID   []byte
 }
 
-func WriteCass() {
-	session, _ := GetCassandraConnection("dp") // create connection to cassandra
-	defer session.Close()
-	url := ""
-
-	iter := session.Query(`SELECT original_url FROM response`).Iter()
-	f, _ := os.OpenFile("dat1.txt", os.O_RDWR|os.O_APPEND, 0666)
-
-	for iter.Scan(&url) {
-		u := []byte(url + "\n")
-		f.Write(u)
-	}
-
-}
-
+// gets names of all departments, checks for mentions in specified time period and returns ranked array of 15 most popular terms and their 30 day frequencies
 func DepartmentsPoliticalActivity() []PoliticalActivity {
 	var dept []Departments // get all departments from postgres sql table
-	var deptNames []string
+	var terms []string
 	err := DB.Find(&dept).Error
 
 	if err != nil && err != gorm.RecordNotFound {
@@ -60,132 +46,70 @@ func DepartmentsPoliticalActivity() []PoliticalActivity {
 	}
 
 	for _, d := range dept {
-		deptNames = append(deptNames, d.GovDept)
+		terms = append(terms, d.GovDept)
 	}
 
-	pa := make([]PoliticalActivity, len(dept))
-	keyEnt := keywords(deptNames)
+	return CheckThese(terms)
+}
 
-	for i, d := range dept {
-		pa[i].Term = d.GovDept
-		for _, ke := range keyEnt {
-			if d.GovDept == ke.Name {
-				dayindex := int((Today.Round(time.Hour).Sub(ke.Date.Round(time.Hour)) / 24).Hours() - 1) // get day index
-				pa[i].Mentions[dayindex]++
+// gets names of all events, checks for mentions in specified time period and returns ranked array of 15 most popular terms and their 30 day frequencies
+func EventsPoliticalActivity() []PoliticalActivity {
+	var event []Events
+	var terms []string
+	err := DB.Find(&event).Error
+
+	if err != nil && err != gorm.RecordNotFound {
+		return nil
+	}
+
+	for _, e := range event {
+		terms = append(terms, e.Event)
+	}
+
+	return CheckThese(terms)
+}
+
+// gets names of all regions, checks for mentions in specified time period and returns ranked array of 15 most popular terms and their 30 day frequencies
+func RegionsPoliticalActivity() []PoliticalActivity {
+	var region []Regions
+	var terms []string
+	err := DB.Find(&region).Error
+
+	if err != nil && err != gorm.RecordNotFound {
+		return nil
+	}
+
+	for _, r := range region {
+		terms = append(terms, r.Town)
+	}
+
+	return CheckThese(terms)
+}
+
+// takes slice of terms, checks for the total number of occurences and returns a top 15 ranked array
+func CheckThese(terms []string) []PoliticalActivity {
+	politicalActivity := make([]PoliticalActivity, len(terms))
+	DatedTerm := keywords(terms) // returns array
+
+	for i, term := range terms { // check each term
+		politicalActivity[i].Term = term // copy to politicalActivity array
+		for _, dt := range DatedTerm {   // check through all dated terms
+			if term == dt.Term { //if there's a match
+				dayindex := int((Today.Round(time.Hour).Sub(dt.Date.Round(time.Hour)) / 24).Hours() - 1) // get day index
+				politicalActivity[i].Mentions[dayindex]++                                                // increase the count for that term on that day
 			}
 		}
 	}
 
-	return RankPA(pa)
+	return RankPA(politicalActivity)
 }
 
-// func EventsPoliticalActivity() []PoliticalActivity {
-// 	var event []Events
-// 	err := DB.Find(&event).Error
-
-// 	if err != nil && err != gorm.RecordNotFound {
-// 		return nil
-// 	}
-
-// 	pa := make([]PoliticalActivity, len(event))
-
-// 	keyEnt := keywords()
-
-// 	for i, e := range event {
-// 		pa[i].Term = e.Event
-// 		for _, ke := range keyEnt {
-// 			if e.Event == ke.Name {
-// 				dayindex := int((Today.Round(time.Hour).Sub(ke.Date.Round(time.Hour)) / 24).Hours() - 1) // get day index
-// 				pa[i].Mentions[dayindex]++
-// 			}
-// 		}
-// 	}
-
-// 	return RankPA(pa)
-// }
-
-// func RegionsPoliticalActivity() []PoliticalActivity {
-// 	var region []Regions
-// 	err := DB.Find(&region).Error
-
-// 	if err != nil && err != gorm.RecordNotFound {
-// 		return nil
-// 	}
-// 	pa := make([]PoliticalActivity, len(region))
-
-// 	keyEnt := keywords()
-
-// 	for i, r := range region {
-// 		pa[i].Term = r.Town
-// 		for _, ke := range keyEnt {
-// 			if r.Town == ke.Name {
-// 				dayindex := int((Today.Round(time.Hour).Sub(ke.Date.Round(time.Hour)) / 24).Hours() - 1) // get day index
-// 				pa[i].Mentions[dayindex]++
-// 			}
-// 		}
-// 	}
-
-// 	return RankPA(pa)
-// }
-
-func GetPoliticalActivityHttp(res http.ResponseWriter, req *http.Request, params martini.Params) string {
-	session := req.Header.Get("X-API-SESSION")
-	if len(session) <= 0 {
-		http.Error(res, "Missing session parameter", http.StatusBadRequest)
-		return "Missing session parameter"
-	}
-
-	var result []PoliticalActivity
-
-	if params["type"] == "d" {
-		result = DepartmentsPoliticalActivity()
-		// } else if params["type"] == "e" {
-		// 	result = EventsPoliticalActivity()
-		// } else if params["type"] == "r" {
-		// 	result = RegionsPoliticalActivity()
-	} else {
-		http.Error(res, "Bad type param", http.StatusInternalServerError)
-		return "Bad type param"
-	}
-
-	r, err := json.Marshal(result)
-	if err != nil {
-		http.Error(res, "Unable to parse JSON", http.StatusInternalServerError)
-		return "Unable to parse JSON"
-	}
-
-	return string(r)
-}
-
-func GetCassandraConnection(keyspace string) (*gocql.Session, error) {
-	cassandraHost := "10.0.0.2"
-	cassandraPort := 49236
-	if os.Getenv("cassandrahost") != "" {
-		cassandraHost = os.Getenv("cassandrahost")
-	}
-	if os.Getenv("cassandraport") != "" {
-		cassandraPort, _ = strconv.Atoi(os.Getenv("cassandraport"))
-	}
-
-	cluster := gocql.NewCluster(cassandraHost)
-	cluster.Port = cassandraPort
-	cluster.Keyspace = keyspace
-	cluster.Consistency = gocql.Quorum
-	session, err := cluster.CreateSession()
-
-	if err != nil {
-		fmt.Println("Could not connect to the Cassandara server.")
-	}
-
-	return session, err
-}
-
-func keywords(names []string) []KeyEnt {
+func keywords(terms []string) []DatedTerm {
 	var id []byte
 	var dateID []string
 	var queryDate time.Time
-	var keyEnt []KeyEnt
-	var tmpKE KeyEnt
+	var DatedTerms []DatedTerm
+	var tmpDT DatedTerm
 
 	session, _ := GetCassandraConnection("dp") // create connection to cassandra
 	defer session.Close()
@@ -200,35 +124,35 @@ func keywords(names []string) []KeyEnt {
 		///return err
 	}
 
-	for _, n := range names {
-		iter := session.Query(`SELECT id FROM keyword WHERE name = ?`, n).Iter()
+	for _, term := range terms {
+		iter := session.Query(`SELECT id FROM keyword WHERE name = ?`, term).Iter()
 		for iter.Scan(&id) {
 			var date time.Time
 			date = DateAndId(id, dateID)
-			if date.Year() < 2 {
-				tmpKE.Name = n
-				tmpKE.ID = id
-				tmpKE.Date = date
-				keyEnt = append(keyEnt, tmpKE)
+			if date.Year() > 1 {
+				tmpDT.Term = term
+				tmpDT.ID = id
+				tmpDT.Date = date
+				DatedTerms = append(DatedTerms, tmpDT)
 			}
 		}
 	}
 
-	for _, n := range names {
-		iter := session.Query(`SELECT id FROM entity WHERE name = ?`, n).Iter()
+	for _, term := range terms {
+		iter := session.Query(`SELECT id FROM entity WHERE name = ?`, term).Iter()
 		for iter.Scan(&id) {
 			var date time.Time
 			date = DateAndId(id, dateID)
-			if date.Year() < 2 {
-				tmpKE.Name = n
-				tmpKE.ID = id
-				tmpKE.Date = date
-				keyEnt = append(keyEnt, tmpKE)
+			if date.Year() > 1 {
+				tmpDT.Term = term
+				tmpDT.ID = id
+				tmpDT.Date = date
+				DatedTerms = append(DatedTerms, tmpDT)
 			}
 		}
 	}
 
-	return keyEnt
+	return DatedTerms
 }
 
 func DateAndId(id []byte, dateID []string) time.Time {
@@ -277,4 +201,70 @@ func RankPA(activities []PoliticalActivity) []PoliticalActivity {
 	}
 
 	return activities[0:15]
+}
+
+func GetPoliticalActivityHttp(res http.ResponseWriter, req *http.Request, params martini.Params) string {
+	session := req.Header.Get("X-API-SESSION")
+	if len(session) <= 0 {
+		http.Error(res, "Missing session parameter", http.StatusBadRequest)
+		return "Missing session parameter"
+	}
+
+	var result []PoliticalActivity
+
+	if params["type"] == "d" {
+		result = DepartmentsPoliticalActivity()
+	} else if params["type"] == "e" {
+		result = EventsPoliticalActivity()
+	} else if params["type"] == "r" {
+		result = RegionsPoliticalActivity()
+	} else {
+		http.Error(res, "Bad type param", http.StatusInternalServerError)
+		return "Bad type param"
+	}
+
+	r, err := json.Marshal(result)
+	if err != nil {
+		http.Error(res, "Unable to parse JSON", http.StatusInternalServerError)
+		return "Unable to parse JSON"
+	}
+
+	return string(r)
+}
+
+func WriteCass() {
+	session, _ := GetCassandraConnection("dp") // create connection to cassandra
+	defer session.Close()
+	url := ""
+
+	iter := session.Query(`SELECT original_url FROM response`).Iter()
+	f, _ := os.OpenFile("dat1.txt", os.O_RDWR|os.O_APPEND, 0666)
+
+	for iter.Scan(&url) {
+		u := []byte(url + "\n")
+		f.Write(u)
+	}
+}
+
+func GetCassandraConnection(keyspace string) (*gocql.Session, error) {
+	cassandraHost := "10.0.0.2"
+	cassandraPort := 49236
+	if os.Getenv("cassandrahost") != "" {
+		cassandraHost = os.Getenv("cassandrahost")
+	}
+	if os.Getenv("cassandraport") != "" {
+		cassandraPort, _ = strconv.Atoi(os.Getenv("cassandraport"))
+	}
+
+	cluster := gocql.NewCluster(cassandraHost)
+	cluster.Port = cassandraPort
+	cluster.Keyspace = keyspace
+	cluster.Consistency = gocql.Quorum
+	session, err := cluster.CreateSession()
+
+	if err != nil {
+		fmt.Println("Could not connect to the Cassandara server.")
+	}
+
+	return session, err
 }
