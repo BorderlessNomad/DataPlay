6c1186c04561d3ae5fbcf3740b1e3545896c85e6
Glyn Dimond <glyn@playgen.com>
2014-10-20 09:39:52 +0100

    Change Cassandra host

--- src/dataplay/politicalactivitymonitor.go
+++ src/dataplay/politicalactivitymonitor.go
@@ -242,8 +242,8 @@ func PopularPoliticalActivity() [3]Popular {
 }
 
 func GetCassandraConnection(keyspace string) (*gocql.Session, error) {
-	cassandraHost := "10.0.0.2"
-	cassandraPort := 49211 //9042
+	cassandraHost := "109.231.121.129"
+	cassandraPort := 9042
 
 	if os.Getenv("DP_CASSANDRA_HOST") != "" {
 		cassandraHost = os.Getenv("DP_CASSANDRA_HOST")
