9c1ae1748849a0fc86b702ea4a76f3cdc8ee47e4
Glyn Dimond <glyn@playgen.com>
2014-10-24 11:38:06 +0100

    news search splits by spaces now

--- src/dataplay/news.go
+++ src/dataplay/news.go
@@ -69,7 +69,7 @@ func SearchForNews(searchstring string) ([]NewsArticle, *appError) {
 	defer session.Close()
 
 	newsArticles := []NewsArticle{}
-	searchTerms := strings.Split(searchstring, "_")
+	searchTerms := strings.Split(searchstring, "%20")
 	earliestDate := Earliest(searchTerms) // links with SQL database
 
 	var date time.Time
