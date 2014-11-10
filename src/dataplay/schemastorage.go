12ac4b5accde959fc44d3c8de86fe8191cd763f7
Mayur Ahir <mayur@playgen.com>
2014-11-10 15:14:57 +0000

    Removed TODO for GetSQLTableSchema.

--- src/dataplay/schemastorage.go
+++ src/dataplay/schemastorage.go
@@ -9,10 +9,6 @@ package main
  * @details Almost all of the SQLs support 'information_schema' database which stores metadata about
  * other databases, tables etc.
  *
- * @todo Apply caching to queries which goes to 'information_schema'
- * MySQL has something like innodb_stats_on_metadata=0 which will prevent statistic update upon quering 'information_schema'.
- * Also it won't make 'information_schema' to be stale when changes are made on corresponding metadata.
- *
  * @param string <Table Name>
  * @return <Table Schema>
  */
