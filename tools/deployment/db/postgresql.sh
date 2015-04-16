b9284f580b0c796da1b3c2ce7951e66539ffc12d
Mayur Ahir <mayur@playgen.com>
2015-04-16 15:56:51 +0100

    PostreSQL send notification to PgPool.

--- tools/deployment/db/postgresql.sh
+++ tools/deployment/db/postgresql.sh
@@ -9,6 +9,12 @@ if [ "$(id -u)" != "0" ]; then
 	exit 1
 fi
 
+APP_HOST=$(ifconfig eth0 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}')
+
+# PGPOOL_API_HOST="109.231.124.145"
+PGPOOL_API_HOST=$(ss-get --timeout 360 pgpool.hostname)
+PGPOOL_API_PORT="1937"
+
 timestamp () {
 	date +"%F %T,%3N"
 }
@@ -43,7 +49,8 @@ setup_database () {
 	createdb -O $DB_USER $DB_NAME
 
 	# Adjust PostgreSQL configuration so that remote connections to the database are possible.
-	# From Private cluster & PlayGen dev IP
+	# From PGPOOL Host, Flexiant clusters & PlayGen dev IP
+	echo "host    all             playgen         $PGPOOL_API_HOST/32        md5" >> /etc/postgresql/$DB_VERSION/main/pg_hba.conf
 	echo "host    all             all             109.231.121.0/24        md5" >> /etc/postgresql/$DB_VERSION/main/pg_hba.conf
 	echo "host    all             all             109.231.122.0/24        md5" >> /etc/postgresql/$DB_VERSION/main/pg_hba.conf
 	echo "host    all             all             109.231.123.0/24        md5" >> /etc/postgresql/$DB_VERSION/main/pg_hba.conf
@@ -52,58 +59,6 @@ setup_database () {
 
 	# And add 'listen_addresses' to '/etc/postgresql/$DB_VERSION/main/postgresql.conf'
 	echo "listen_addresses='*'" >> /etc/postgresql/$DB_VERSION/main/postgresql.conf
-
-	service postgresql restart
-}
-
-setup_cluster_access() {
-	DB_VERSION="9.4"
-	DB_HOST="localhost"
-	DB_PORT="5432"
-	DB_USER="playgen"
-	DB_PASSWORD="aDam3ntiUm"
-	DB_NAME="dataplay"
-	PGPOOL_VERSION="3.4.2"
-
-	cp /var/lib/postgresql/.pgpass ~/.pgpass
-
-	mkdir ~/pgpool-local
-
-	CLUSTER_IP="109.231.124.105" # Replace with Cluster IP
-	echo "host    all             playgen         $CLUSTER_IP/32      md5" >> /etc/postgresql/$DB_VERSION/main/pg_hba.conf
-
-	service postgresql restart
-
-	apt-get install -y postgresql-9.4-pgpool2
-
-	#apt-get install -y build-essential
-
-	wget http://www.pgpool.net/download.php?f=pgpool-II-$PGPOOL_VERSION.tar.gz -O pgpool-II-$PGPOOL_VERSION.tar.gz
-
-	tar -xvzf pgpool-II-$PGPOOL_VERSION.tar.gz
-
-	cp pgpool-II-$PGPOOL_VERSION/src/sql/pgpool_adm/pgpool_adm.sql.in ~/pgpool-local/pgpool_adm.sql
-	cp pgpool-II-$PGPOOL_VERSION/src/sql/pgpool-recovery/pgpool-recovery.sql.in ~/pgpool-local/pgpool-recovery.sql
-	cp pgpool-II-$PGPOOL_VERSION/src/sql/pgpool-regclass/pgpool-regclass.sql.in ~/pgpool-local/pgpool-regclass.sql
-
-	###
-	# ls -al /usr/lib/postgresql/9.4/lib/ | grep 'pgpool'
-	# -rw-r--r-- 1 root root   14432 Nov  6 21:15 pgpool_adm.so
-	# -rw-r--r-- 1 root root   14040 Nov  6 21:15 pgpool-recovery.so
-	# -rw-r--r-- 1 root root    9944 Nov  6 21:15 pgpool-regclass.so
-	###
-	sed -i "s/MODULE_PATHNAME/\/usr\/lib\/postgresql\/$DB_VERSION\/lib\/pgpool_adm/g" ~/pgpool-local/pgpool_adm.sql
-	# Note: error on line # 45 & 51 should retrun integer
-	sed -i "43,51s/record/integer/" ~/pgpool-local/pgpool_adm.sql
-
-	sed -i "s/MODULE_PATHNAME/\/usr\/lib\/postgresql\/$DB_VERSION\/lib\/pgpool-recovery/g" ~/pgpool-local/pgpool-recovery.sql
-	sed -i "s/\$libdir\/pgpool-recovery/\/usr\/lib\/postgresql\/$DB_VERSION\/lib\/pgpool-recovery/g" ~/pgpool-local/pgpool-recovery.sql
-
-	sed -i "s/MODULE_PATHNAME/\/usr\/lib\/postgresql\/$DB_VERSION\/lib\/pgpool-regclass/g" ~/pgpool-local/pgpool-regclass.sql
-
-	psql -h $DB_HOST -U $DB_USER -d $DB_NAME -f ~/pgpool-local/pgpool_adm.sql
-	psql -h $DB_HOST -U $DB_USER -d $DB_NAME -f ~/pgpool-local/pgpool-recovery.sql
-	psql -h $DB_HOST -U $DB_USER -d $DB_NAME -f ~/pgpool-local/pgpool-regclass.sql
 
 	service postgresql restart
 }
@@ -151,6 +106,14 @@ update_iptables () {
 	iptables-save
 }
 
+inform_pgpool () {
+	retries=0
+	until curl -H "Content-Type: application/json" -X POST -d "{\"ip\":\"$APP_HOST:\"}" http://$PGPOOL_API_HOST:$PGPOOL_API_PORT; do
+		echo "[$(timestamp)] Load Balancer is not up yet, retry... [$(( retries++ ))]"
+		sleep 5
+	done
+}
+
 echo "[$(timestamp)] ---- 1. Setup Host ----"
 setuphost
 
@@ -163,8 +126,8 @@ su postgres -c "$(typeset -f setup_database); setup_database" # Run function as 
 echo "[$(timestamp)] ---- 4. Import Data ----"
 su postgres -c "$(typeset -f import_data); import_data" # Run function as user 'postgres'
 
-echo "[$(timestamp)] ---- 5. Setup PostgresSQL with pgpool-II ----"
-setup_cluster_access
+echo "[$(timestamp)] ---- 5. Inform pgpool (Add) ----"
+inform_pgpool
 
 echo "[$(timestamp)] ---- 6. Update IPTables rules ----"
 update_iptables
