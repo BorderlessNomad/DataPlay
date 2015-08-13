7be8b1621a54872af06b934f846abe9248f695b2
Mayur Ahir <mayur@playgen.com>
2015-08-13 16:20:23 +0100

    Enable connection pooling for Redis.

--- src/dataplay/redis.go
+++ src/dataplay/redis.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	// "github.com/mediocregopher/radix.v2/pool"
+	"github.com/mediocregopher/radix.v2/pool"
 	"github.com/mediocregopher/radix.v2/redis"
 	"os"
 	"strconv"
-	"time"
 )
 
 func GetRedisConnection() (client *redis.Client, err error) {
 	redisHost := "109.231.121.62"
 	redisPort := "6379"
-	redisTimeout := time.Duration(10) * time.Second
+	redisPoolSize := 100
 
 	if os.Getenv("DP_REDIS_HOST") != "" {
 		redisHost = os.Getenv("DP_REDIS_HOST")
@@ -21,18 +20,25 @@ func GetRedisConnection() (client *redis.Client, err error) {
 		redisPort = os.Getenv("DP_REDIS_PORT")
 	}
 
-	if os.Getenv("DP_REDIS_TIMEOUT") != "" {
-		timeout, _ := strconv.Atoi(os.Getenv("DP_REDIS_TIMEOUT"))
-		redisTimeout = time.Duration(timeout) * time.Second
+	if os.Getenv("DP_REDIS_POOLSIZE") != "" {
+		redisPoolSize, _ = strconv.Atoi(os.Getenv("DP_REDIS_POOLSIZE"))
 	}
 
 	Logger.Println("Connecting to Redis " + redisHost + ":" + redisPort)
-	client, err = redis.DialTimeout("tcp", redisHost+":"+redisPort, redisTimeout)
 
+	p, err := pool.New("tcp", redisHost+":"+redisPort, redisPoolSize)
 	if err != nil {
-		Logger.Println("Could not connect to the redis server.")
+		Logger.Println("Could not initiate Redis Pool.")
 		return nil, err
 	}
+
+	client, err = p.Get()
+	if err != nil {
+		Logger.Println("Could not connect to the Redis server.")
+		return nil, err
+	}
+
+	defer p.Put(client)
 
 	Logger.Println("Connected!")
 
